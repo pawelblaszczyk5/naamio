86da3dffe77c2e31d1e144e88e432f17dfe3cda0
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-11-02 19:24:49 +0100

    chore: fix ci

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -4,6 +4,7 @@ import { definePreview } from "@storybook/react-vite";
 
 import "#src/styles.css";
 
+// cspell:ignore autodocs
 const preview = definePreview({ addons: [addonA11y(), addonDocs()], parameters: {}, tags: ["autodocs"] });
 
 export default preview;

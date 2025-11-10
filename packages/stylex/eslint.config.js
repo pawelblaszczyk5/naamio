276b51f67c07deb3a091111f2d3c2337f881c4eb
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-11-10 20:41:56 +0100

    chore: remove unnecessary config field

--- packages/stylex/eslint.config.js
+++ packages/stylex/eslint.config.js
@@ -6,7 +6,6 @@ export default defineConfig(
 	{
 		languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname } },
 		extends: [core],
-		files: ["**/*.{ts,tsx,js,jsx}"],
 	},
 	{ files: ["src/mod.ts"], rules: { "import-x/no-default-export": "off" } },
 );

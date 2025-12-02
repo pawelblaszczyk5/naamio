8d23310f4a1188e6bea8e2a7e30d1dc25c25060f
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-12-02 20:20:00 +0100

    feat: migrate ports to match dev-preview

--- apps/gaia/server.ts
+++ apps/gaia/server.ts
@@ -25,4 +25,4 @@ app.use("/assets/*", cache({ immutable: true, seconds: ONE_YEAR_IN_SECONDS }), s
 app.use("*", cache({ immutable: false, seconds: ONE_HOUR_IN_SECONDS }), serveStatic({ root: "./dist/client" }));
 app.use("*", async (ctx) => handler.fetch(ctx.req.raw) as Promise<Response>);
 
-serve({ fetch: app.fetch, port: 5_200 });
+serve({ fetch: app.fetch, port: 6_200 });

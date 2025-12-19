0a299c6b0afd10a62d56f1e54541ba4dec8435e1
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-12-19 14:57:12 +0100

    chore: change order of generated messages in catalogs

--- apps/gaia/lingui.config.ts
+++ apps/gaia/lingui.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "@lingui/conf";
 export default defineConfig({
 	catalogs: [{ include: ["src"], path: "<rootDir>/src/locales/{locale}" }],
 	locales: ["pl-PL", "en-US"],
+	orderBy: "origin",
 	sourceLocale: "en-US",
 });

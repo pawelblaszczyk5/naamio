67d7a02c30924a26bcc10b4abfc44e1eec56a219
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-12-28 19:23:24 +0100

    feat: initial sketch of user service

---
+++ apps/mercury/src/modules/user/mod.ts
@@ -0,0 +1,64 @@
+import type { Option } from "effect";
+
+import { SqlSchema } from "@effect/sql";
+import { PgClient } from "@effect/sql-pg";
+import { Context, Effect, Layer } from "effect";
+
+import { generateId } from "@naamio/id-generator/effect";
+import { UserModel } from "@naamio/schema";
+
+import { DatabaseLive } from "#src/modules/database/mod.js";
+
+export class User extends Context.Tag("@naamio/mercury/User")<
+	User,
+	{
+		system: {
+			create: (email: UserModel["email"]) => Effect.Effect<UserModel["id"]>;
+			findByEmail: (email: UserModel["email"]) => Effect.Effect<Option.Option<Pick<UserModel, "email" | "id">>>;
+		};
+	}
+>() {
+	static Live = Layer.effect(
+		this,
+		Effect.gen(function* () {
+			const sql = yield* PgClient.PgClient;
+
+			const insertUser = SqlSchema.void({
+				execute: (request) => sql`
+					INSERT INTO
+						${sql("user")} ${sql.insert(request)};
+				`,
+				Request: UserModel.insert,
+			});
+
+			const findByEmail = SqlSchema.findOne({
+				execute: (request) => sql`
+					SELECT
+						${sql("id")},
+						${sql("email")}
+					FROM
+						${sql("user")}
+					WHERE
+						${sql("email")} = ${request}
+				`,
+				Request: UserModel.fields.email,
+				Result: UserModel.select.pick("id", "email"),
+			});
+
+			return {
+				system: {
+					create: Effect.fn("@naamio/mercury/User#create")(function* (email) {
+						const id = UserModel.fields.id.make(yield* generateId());
+
+						yield* insertUser({ createdAt: undefined, email, id }).pipe(Effect.orDie);
+
+						return id;
+					}),
+					findByEmail: Effect.fn("@naamio/mercury/User#findByEmail")(function* (email) {
+						return yield* findByEmail(email).pipe(Effect.orDie);
+					}),
+				},
+			} satisfies User["Type"];
+		}),
+	).pipe(Layer.provide(DatabaseLive));
+}

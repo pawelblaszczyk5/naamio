0fe9fd62c63525a2f5fe562009100be4adf8b530
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-12-28 10:27:08 +0100

    feat: add schema package with initial schemas for DB entities related to auth

---
+++ packages/schema/README.md
@@ -0,0 +1,3 @@
+# @naamio/schema
+
+Package responsible for storing schema of various entities and domain objects across the Naamio apps. It uses `effect` schema which can be reused across frontend and backend and also used to store additional annotations.
---
+++ packages/schema/src/mod.ts
@@ -0,0 +1,28 @@
+import { Model } from "@effect/sql";
+import { Schema } from "effect";
+
+const Id = Schema.Trimmed.pipe(Schema.length(16));
+
+export class UserModel extends Model.Class<UserModel>("@naamio/schema/User")({
+	createdAt: Model.DateTimeInsertFromDate,
+	email: Schema.NonEmptyTrimmedString,
+	id: Model.GeneratedByApp(Id.pipe(Schema.brand("UserId"))),
+}) {}
+
+export class SessionModel extends Model.Class<SessionModel>("@naamio/schema/Session")({
+	createdAt: Model.DateTimeInsertFromDate,
+	expiresAt: Model.DateTimeFromDate,
+	id: Model.GeneratedByApp(Id.pipe(Schema.brand("SessionId"))),
+	revokedAt: Model.FieldOption(Model.DateTimeFromDate),
+	userId: UserModel.fields.id,
+}) {}
+
+export class EmailChallengeModel extends Model.Class<EmailChallengeModel>("@naamio/schema/EmailChallenge")({
+	createdAt: Model.DateTimeInsertFromDate,
+	email: UserModel.fields.email,
+	expiresAt: Model.DateTimeFromDate,
+	hash: Schema.NonEmptyTrimmedString.pipe(Schema.Redacted),
+	id: Model.GeneratedByApp(Id.pipe(Schema.brand("EmailChallengeId"))),
+	revokedAt: Model.FieldOption(Model.DateTimeFromDate),
+	state: Schema.NonEmptyTrimmedString.pipe(Schema.Redacted),
+}) {}

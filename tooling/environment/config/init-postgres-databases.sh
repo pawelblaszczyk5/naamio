fa0dc6b2f876527765afe83ad66605b6dea0d412
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2026-01-06 20:49:10 +0100

    feat: initial setup for electric

--- tooling/environment/config/init-postgres-databases.sh
+++ tooling/environment/config/init-postgres-databases.sh
@@ -4,9 +4,11 @@ set -e
 # cspell:ignore psql EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE USER naamio_cluster WITH PASSWORD 'example_cluster_password_123';
+  CREATE ROLE naamio_cluster WITH LOGIN PASSWORD 'example_cluster_password_123';
   CREATE DATABASE naamio_cluster OWNER naamio_cluster;
 
-  CREATE USER naamio_app WITH PASSWORD 'example_app_password_123';
+  CREATE ROLE naamio_app WITH LOGIN PASSWORD 'example_app_password_123';
   CREATE DATABASE naamio_app OWNER naamio_app;
+
+  CREATE ROLE naamio_electric WITH SUPERUSER LOGIN PASSWORD 'example_electric_password_123';
 EOSQL

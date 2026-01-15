67e73a7d4175aec119a332602185481d09923aed
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2026-01-15 22:09:25 +0100

    chore: start increasing effect diagnostics severity

--- packages/api/src/errors/mod.ts
+++ packages/api/src/errors/mod.ts
@@ -1,19 +1,19 @@
 import { HttpApiSchema } from "@effect/platform";
 import { Schema } from "effect";
 
-export class TooManyRequests extends Schema.TaggedError<TooManyRequests>()(
+export class TooManyRequests extends Schema.TaggedError<TooManyRequests>("@naamio/api/TooManyRequests")(
 	"TooManyRequests",
 	{},
 	HttpApiSchema.annotations({ status: 429 }),
 ) {}
 
-export class InsufficientStorage extends Schema.TaggedError<InsufficientStorage>()(
+export class InsufficientStorage extends Schema.TaggedError<InsufficientStorage>("@naamio/api/InsufficientStorage")(
 	"InsufficientStorage",
 	{},
 	HttpApiSchema.annotations({ status: 507 }),
 ) {}
 
-export class BadGateway extends Schema.TaggedError<BadGateway>("BadGateway")(
+export class BadGateway extends Schema.TaggedError<BadGateway>("@naamio/api/BadGateway")(
 	"BadGateway",
 	{},
 	HttpApiSchema.annotations({ status: 502 }),

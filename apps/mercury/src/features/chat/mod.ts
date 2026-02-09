05d87957d1149b6a4caf2d3207c42173cac55915
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2026-02-09 21:11:18 +0100

    feat(mercury): add errors to chat service sketch

--- apps/mercury/src/features/chat/mod.ts
+++ apps/mercury/src/features/chat/mod.ts
@@ -1,6 +1,6 @@
 import type { Effect } from "effect";
 
-import { Context } from "effect";
+import { Context, Schema } from "effect";
 
 import type { CurrentSession } from "@naamio/api/middlewares/authenticated-only";
 import type {
@@ -34,6 +34,18 @@ interface AgentMessage {
 	id: AgentMessageModel["id"];
 }
 
+export class MissingConversationError extends Schema.TaggedError<MissingConversationError>(
+	"@naamio/mercury/Chat/MissingConversationError",
+)("MissingConversationError", {}) {}
+
+export class MissingMessageError extends Schema.TaggedError<MissingMessageError>(
+	"@naamio/mercury/Chat/MissingMessageError",
+)("MissingMessageError", {}) {}
+
+export class EmptyUserMessageError extends Schema.TaggedError<EmptyUserMessageError>(
+	"@naamio/mercury/Chat/EmptyUserMessageError",
+)("EmptyUserMessageError", {}) {}
+
 export class Chat extends Context.Tag("@naamio/mercury/Chat")<
 	Chat,
 	{
@@ -41,16 +53,24 @@ export class Chat extends Context.Tag("@naamio/mercury/Chat")<
 			continueConversation: (data: {
 				conversationId: ConversationModel["id"];
 				newMessages: [UserMessage, AgentMessage];
-			}) => Effect.Effect<{ transactionId: TransactionId }, never, CurrentSession>;
+			}) => Effect.Effect<
+				{ transactionId: TransactionId },
+				EmptyUserMessageError | MissingConversationError,
+				CurrentSession
+			>;
 			regenerateAnswer: (data: {
 				conversationId: ConversationModel["id"];
 				messageId: UserMessageModel["id"];
 				newMessage: AgentMessage;
-			}) => Effect.Effect<{ transactionId: TransactionId }, never, CurrentSession>;
+			}) => Effect.Effect<
+				{ transactionId: TransactionId },
+				MissingConversationError | MissingMessageError,
+				CurrentSession
+			>;
 			startConversation: (data: {
 				conversationId: ConversationModel["id"];
 				initialMessages: [RootUserMessage, AgentMessage];
-			}) => Effect.Effect<{ transactionId: TransactionId }, never, CurrentSession>;
+			}) => Effect.Effect<{ transactionId: TransactionId }, EmptyUserMessageError, CurrentSession>;
 		};
 	}
 >() {}

ad6dda1991de675e106a07a80d580a9806c253f5
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2026-03-01 14:35:06 +0100

    chore(vulcan): fix expired note

--- apps/vulcan/.storybook/main.ts
+++ apps/vulcan/.storybook/main.ts
@@ -1,7 +1,6 @@
 import { defineMain } from "@storybook/react-vite/node";
 
-// NOTE: this is typed as unknown, because it uses some unused types and I don't need it anyway
-const main: unknown = defineMain({
+const main = defineMain({
 	addons: ["@storybook/addon-docs", "@storybook/addon-a11y"],
 	framework: { name: "@storybook/react-vite", options: { strictMode: true } },
 	stories: ["../src/stories/**/*.stories.@(ts|tsx)"],

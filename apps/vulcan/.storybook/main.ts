8ca087e92b2d2f1591f7682468a931dbe9539b4f
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2026-03-06 23:20:37 +0100

    fix(vulcan): properly run storybook behind Caddy

--- apps/vulcan/.storybook/main.ts
+++ apps/vulcan/.storybook/main.ts
@@ -2,20 +2,9 @@ import { defineMain } from "@storybook/react-vite/node";
 
 const main = defineMain({
 	addons: ["@storybook/addon-docs", "@storybook/addon-a11y"],
+	core: { allowedHosts: ["vulcan.naamio.orb.local"] },
 	framework: { name: "@storybook/react-vite", options: { strictMode: true } },
 	stories: ["../src/stories/**/*.stories.@(ts|tsx)"],
-	viteFinal: (config) => {
-		if (config.server) {
-			config.server.allowedHosts = ["vulcan.naamio.orb.local"];
-
-			if (typeof config.server.hmr === "object") {
-				// eslint-disable-next-line fp/no-delete -- I'm not 100% sure why Storybook is setting this, this makes it impossible to work behind reverse proxy
-				delete config.server.hmr.port;
-			}
-		}
-
-		return config;
-	},
 });
 
 export default main;

24adbdaf968ad5e5e277453475773aba05e0c521
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2026-03-06 23:24:20 +0100

    fix(vulcan): fix HMR behind Caddy

--- apps/vulcan/.storybook/main.ts
+++ apps/vulcan/.storybook/main.ts
@@ -5,6 +5,14 @@ const main = defineMain({
 	core: { allowedHosts: ["vulcan.naamio.orb.local"] },
 	framework: { name: "@storybook/react-vite", options: { strictMode: true } },
 	stories: ["../src/stories/**/*.stories.@(ts|tsx)"],
+	viteFinal: (config) => {
+		if (config.server && typeof config.server.hmr === "object") {
+			// eslint-disable-next-line fp/no-delete -- I'm not 100% sure why Storybook is setting this, this makes it impossible to work behind reverse proxy
+			delete config.server.hmr.port;
+		}
+
+		return config;
+	},
 });
 
 export default main;

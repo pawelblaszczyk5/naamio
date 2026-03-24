d6f8fafb2fb653405faacb74606abbcfce9820dd
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2026-03-24 21:48:32 +0100

    fix(janus): remove unnecessary ts-expect-error directive after updating

--- apps/janus/src/lib/api-client/mod.ts
+++ apps/janus/src/lib/api-client/mod.ts
@@ -51,7 +51,6 @@ export class NaamioUrlBuilder extends ServiceMap.Service<
 	static layer = Layer.effect(
 		this,
 		Effect.gen(function* () {
-			// @ts-expect-error -- this must be fixed, report issue
 			return NaamioUrlBuilder.of(HttpApiClient.urlBuilder(NaamioApi));
 		}),
 	) satisfies Layer.Layer<NaamioUrlBuilder, unknown>;
